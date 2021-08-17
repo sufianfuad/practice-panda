6f413673645174bd1e8e0a792d05f993c7713e92
sufianfuad <sufianfuad2611@gmail.com>
2021-08-18 02:00:41 +0600

    event listener  added

---
+++ js/custom.js
@@ -0,0 +1,11 @@
+document.getElementById('submit-button').addEventListener('click', function () {
+    console.log('Clicked Buy now button');
+
+    document.getElementById('subscribe-title').innerHTML = 'YaY! I am Now Control It'
+
+    // const subscribeUser = document.getElementById('subscribe-title');
+    // const subscribeUserText = subscribeUser.innerText;
+    // const subscribeTitle = parseInt(subscribeUserText);
+
+    // subscribeUser.innerText = subscribeTitle;
+})
